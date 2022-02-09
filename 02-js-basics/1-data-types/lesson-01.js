bc629c06aea2cb8e986380a96ebbb9e78bbef195
HungOm <hung.workspace@gmail.com>
2022-02-09 14:48:44 +0800

    initial lessons

---
+++ 02-js-basics/1-data-types/lesson-01.js
@@ -0,0 +1,11 @@
+var name;
+
+name= "John";
+name = "James"
+const PI = 3.14
+// car should make year
+// model Number
+// Brand 
+
+// let doc = document.getElementById('result')
+// doc.innerHTML = name
